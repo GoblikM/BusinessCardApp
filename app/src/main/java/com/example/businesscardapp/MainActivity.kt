da3244dfee375c3886e0e9e4297ed68b3537f05f
m_goldbach <m_goldbach@utb.cz>
2023-10-22 21:16:28 +0200

    logo pulse animation, social icons

--- app/src/main/java/com/example/businesscardapp/MainActivity.kt
+++ app/src/main/java/com/example/businesscardapp/MainActivity.kt
@@ -1,39 +1,43 @@
 package com.example.businesscardapp
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Email
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -44,7 +48,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            BusinessCardAppTheme() {
+            BusinessCardAppTheme(darkTheme = true) {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -58,25 +62,40 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
 @Composable
 fun BusinessCard(name: String, modifier: Modifier = Modifier) {
+    val infiniteTransition = rememberInfiniteTransition(label = "pulse")
+    val scale by infiniteTransition.animateFloat(
+        initialValue = 1.0f,
+        targetValue = 1.1f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(1000, easing = LinearEasing),
+            repeatMode = RepeatMode.Reverse
+        ), label = "logo_pulse"
+    )
     Column(
         verticalArrangement = Arrangement.SpaceEvenly,
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier.padding(all = 10.dp)
     ) {
-        Image(
-            painterResource(id = R.drawable.logo),
-            colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSurface),
-            contentDescription = "Cards icon",
+        Box(
             modifier = Modifier
-                .clip(RoundedCornerShape(16.dp))
-                .border(
-                    5.dp,
-                    MaterialTheme.colorScheme.onSurface,
-                    shape = RoundedCornerShape(16.dp)
-                )
-        )
+                .scale(scale, scale)
+        ){
+            Image(
+                painterResource(id = R.drawable.logo),
+                colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSurface),
+                contentDescription = "Cards icon",
+                modifier = Modifier
+                    .clip(RoundedCornerShape(16.dp))
+                    .border(
+                        5.dp,
+                        MaterialTheme.colorScheme.onSurface,
+                        shape = RoundedCornerShape(16.dp)
+                    )
+            )
+        }
         Column(
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
@@ -98,6 +117,7 @@ fun BusinessCard(name: String, modifier: Modifier = Modifier) {
 
 }
 
+
 @Composable
 fun ContactsCard(modifier: Modifier = Modifier) {
     val configuration = LocalConfiguration.current
@@ -112,81 +132,57 @@ fun ContactsCard(modifier: Modifier = Modifier) {
 
 
     ) {
-        Column(
+        Row(
             modifier = Modifier
-                .fillMaxHeight()
-                .fillMaxWidth(),
-            verticalArrangement = Arrangement.SpaceEvenly,
-            horizontalAlignment = Alignment.CenterHorizontally
+                .fillMaxSize()
+                .padding(horizontal = 30.dp),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
         )
         {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-
-                ) {
-                    Icon(
-                        imageVector = Icons.Default.Email,
-                        contentDescription = "email icon",
-                        tint = MaterialTheme.colorScheme.onSecondaryContainer,
-                        modifier = Modifier.size(24.dp)
-                    )
-                    Text(
-                        text = "m_goldbach@utb.cz",
-
-                    )
-
-                }
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-
-                    ) {
-                    Image(
-                        painterResource(id = R.drawable.fb_icon),
-                        colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSecondaryContainer),
-                        contentDescription = "email icon",
-                        modifier = Modifier.size(24.dp)
-                    )
-                    Spacer(modifier = Modifier.width(7.dp))
-                    Text(
-                        text = "@m_goldbach",
-                        fontFamily = FontFamily.Monospace
-                    )
-                }
-
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-
-                    ) {
-                    Image(
-                        painterResource(id = R.drawable.ig_icon),
-                        colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSecondaryContainer),
-                        contentDescription = "email icon",
-                        modifier = Modifier.size(24.dp)
-                    )
-                    Spacer(modifier = Modifier.width(7.dp))
-                    Text(
-                        text = "@m_goldbach",
-                        fontFamily = FontFamily.Monospace
-                    )
-
-                }
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-
+            // Column of social icons
+                Column(
+                    modifier = Modifier
+                        .fillMaxHeight(),
+                    verticalArrangement = Arrangement.SpaceEvenly
                 ) {
                     Image(
-                        painterResource(id = R.drawable.x_icon),
-                        colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSecondaryContainer),
+                        painterResource(id = R.drawable.outlook),
                         contentDescription = "email icon",
                         modifier = Modifier.size(24.dp)
                     )
-                    Spacer(modifier = Modifier.width(7.dp))
-                    Text(
-                        text = "@m_goldbach",
-                        fontFamily = FontFamily.Monospace
-
+                    Image(
+                        painterResource(id = R.drawable.fb),
+                        contentDescription = "fb icon",
+                        modifier = Modifier.size(24.dp)
                     )
+                    Image(
+                        painterResource(id = R.drawable.ig),
+                        contentDescription = "ig icon",
+                        modifier = Modifier.size(24.dp)
+                    )
+                    Image(
+                        painterResource(id = R.drawable.x),
+                        contentDescription = "x icon",
+                        modifier = Modifier.size(24.dp)
+                    )
+
                 }
+            Column(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .padding(horizontal = 10.dp),
+
+                verticalArrangement = Arrangement.SpaceEvenly
+            )
+            {
+                Text(text = "m_goldbach@utb.cz")
+                Text(text = "@m_goldbach")
+                Text(text = "@m_goldbach")
+                Text(text = "@m_goldbach")
+
+            }
+
         }
     }
 
@@ -198,7 +194,12 @@ fun ContactsCardPrev() {
     ContactsCard()
 }
 
-@Preview(showBackground = true)
+@Preview
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark mode"
+)
 @Composable
 fun LogoPreview() {
     BusinessCardAppTheme {
